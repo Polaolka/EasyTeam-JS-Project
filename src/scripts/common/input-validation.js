fcf9ccdc78cc57dbb6e086c6b9bb38d8557f1dd1
Paul <pawel.graur47@gmail.com>
2023-03-09 13:07:45 +0200

    Add input-validation

--- src/scripts/common/input-validation.js
+++ src/scripts/common/input-validation.js
@@ -1,13 +1,38 @@
-const emailInput = document.querySelector('.subscription__input');
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const reg1 = /^[\w-.]+@([\w-]+.)+[\w-]{2,4}$/;
+const input = document.querySelector('#input');
+const message = document.querySelector('.subscription__message');
+const btn = document.querySelector('.subscription__btn');
+const userData = {};
 
-emailInput.addEventListener('input', function() {
-  const emailValue = emailInput.value;
-  if (emailRegex.test(emailValue)) {
-    emailInput.classList.add('valid');
-    emailInput.classList.remove('invalid');
+btn.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (!validateEmail(input.value, reg1)) {
+    notValidateEmail(input, message, '');
+    alert(`You have entered incorrect information. Please enter a valid email address, e.g. example@gmail.com`);
   } else {
-    emailInput.classList.add('invalid');
-    emailInput.classList.remove('valid');
+    validEmail(input, message, '');
+    userData.userEmail = input.value;
+    input.value = '';
+    alert(`Thank you for subscribing with email: ${userData.userEmail}`);
   }
-});
+}); 
+
+function validateEmail(input, regex) {
+  return regex.test(input);
+}
+
+function notValidateEmail(input, element, message) {
+  input.classList.add('is-invalid');
+  input.classList.remove('is-valid');
+  element.innerHTML = message;
+}
+
+function validEmail(input, element, message) {
+  input.classList.remove('is-invalid');
+  input.classList.add('is-valid');
+  element.innerHTML = message;
+  setTimeout(function () {
+    input.classList.remove('is-valid');
+  }, 3000);
+}
--- src/scripts/home.js
+++ src/scripts/home.js
@@ -8,6 +8,7 @@ import './common/theme-toggler';
 import './common/theme-toggler-mobile';
 import './modals/open-close-modal';
 import './common/scroll-too-top';
+import './common/input-validation';
 import './common/footer';
 
 import './common/add-to-favorites';
