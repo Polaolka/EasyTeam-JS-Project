e192a8e4778fab22d85773153e2cf547c24e2db8
AnastasiiaHM <119488781+AnastasiiaHM@users.noreply.github.com>
2023-03-09 11:39:25 +0100

    add fixed btn add

--- src/scripts/render/render.js
+++ src/scripts/render/render.js
@@ -1,22 +1,24 @@
-import icons from "../../images/icons.svg"
+import icons from '../../images/icons.svg';
 
 const galleryEl = document.querySelector('.gallery__wrapper');
-import icons from "../../images/icons.svg"
-
+import icons from '../../images/icons.svg';
 
 export default class Render {
-    constructor() { }
-    // Рендеримо галерею
-    renderGallery(data) {
-        const galleryItems = data.map(({ strDrinkThumb, strDrink, idDrink }) => {
-            const lockalStorageItems = JSON.parse(localStorage.getItem('favIds'));
-            const isInLS = lockalStorageItems.includes(idDrink);
-            const className = isInLS ? 'fav-buttons__btn--remove' : 'buttons__btn--add-to';
-            const btnText = isInLS
-                ? `Remove<svg class="buttons__icon"><usehref="${icons}#heart"></usehref=></svg>`
-                : `Add to<svgclass="buttons__icon"><use href="${icons}#heart"></use></>`;
+  constructor() {}
+  // Рендеримо галерею
+  renderGallery(data) {
+    const galleryItems = data
+      .map(({ strDrinkThumb, strDrink, idDrink }) => {
+        const lockalStorageItems = JSON.parse(localStorage.getItem('favIds'));
+        const isInLS = lockalStorageItems.includes(idDrink);
+        const className = isInLS
+          ? 'fav-buttons__btn--remove'
+          : 'buttons__btn--add-to';
+        const btnText = isInLS
+          ? `Remove<svg class="buttons__icon"><usehref="${icons}#icon-heart_fill"></usehref=></svg>`
+          : `Add to<svgclass="buttons__icon"><use href="${icons}#heart"></use></>`;
 
-            return `<div class="card" id="${idDrink}">
+        return `<div class="card" id="${idDrink}">
                 <img src="${strDrinkThumb}" alt="${strDrink}" class="card__img">
                 <h3 class="card__title">${strDrink}</h3>
                 <div class="buttons">
@@ -31,18 +33,18 @@ export default class Render {
                     </button>
                 </div>
             </div>`;
-        })
-            .join('');
+      })
+      .join('');
 
-        galleryEl.insertAdjacentHTML('beforeend', galleryItems);
-    }
+    galleryEl.insertAdjacentHTML('beforeend', galleryItems);
+  }
 
-    createNotFoundMarkup() {
-        return `<div class="not-found">
+  createNotFoundMarkup() {
+    return `<div class="not-found">
         <h2 class="not-found__title">Sorry, we didn't find any cocktail for you</h2>
         <svg class="not-found__icon">
         <use href="${icons}#icon-sorry"></use>
         </svg>
         </div>`;
-    }
+  }
 }
