605187e32cded006f93f5753d93137cc83d0fe44
Oleksii Savchenko <savchenko.orient@gmail.com>
2023-03-09 12:59:29 +0200

    fix fix fix!

--- src/scripts/gallery/gallery.js
+++ src/scripts/gallery/gallery.js
@@ -190,60 +190,60 @@ async function handleHomeLoad() {
 
     // Вішаємо слухачі
     // addListener();
-// ++++++++++++
-galleryEl.addEventListener('click', e => {
-    const elem = e.target;
-  
-    const addToBtn = elem.closest('.buttons__btn--add-to');
-    const removeBtn = elem.closest('.fav-buttons__btn--remove')
-    if (elem.classList.contains('buttons__btn--learn-more')) {
-      handleOpenCloseModal(e);
-    }
-    if (addToBtn) {
-      addToBtn.classList.remove('buttons__btn--add-to');
-      addToBtn.classList.add('fav-buttons__btn--remove');
-  
-      addToBtn.innerHTML = `Remove<svg class="buttons__icon"><use href="${icons}#heart"></use></svg>`;
-      favGallery.addToFavorite(addToBtn);
-    }
-    if (removeBtn) {
-      console.log('removeBtn: ', removeBtn);
-      removeBtn.classList.remove('fav-buttons__btn--remove')
-      removeBtn.classList.add('buttons__btn--add-to');
-  
-      removeBtn.innerHTML = `Add to<svg class="buttons__icon"><use href="${icons}#heart"></use></>`;
-      favGallery.removeFromFavoriteFromGallery(removeBtn);
-    }
-  });
+    // ++++++++++++
+    galleryEl.addEventListener('click', e => {
+      const elem = e.target;
+
+      const addToBtn = elem.closest('.buttons__btn--add-to');
+      const removeBtn = elem.closest('.fav-buttons__btn--remove')
+      if (elem.classList.contains('buttons__btn--learn-more')) {
+        handleOpenCloseModal(e);
+      }
+      if (addToBtn) {
+        addToBtn.classList.remove('buttons__btn--add-to');
+        addToBtn.classList.add('fav-buttons__btn--remove');
+
+        addToBtn.innerHTML = `Remove<svg class="buttons__icon"><use href="${icons}#icon-heart_fill"></use></svg>`;
+        favGallery.addToFavorite(addToBtn);
+      }
+      if (removeBtn) {
+        console.log('removeBtn: ', removeBtn);
+        removeBtn.classList.remove('fav-buttons__btn--remove')
+        removeBtn.classList.add('buttons__btn--add-to');
+
+        removeBtn.innerHTML = `Add to<svg class="buttons__icon"><use href="${icons}#heart"></use></>`;
+        favGallery.removeFromFavoriteFromGallery(removeBtn);
+      }
+    });
   } else {
     gallery.getRandomData();
     // addListener();
-// +++++++++++++++++     //
-galleryEl.addEventListener('click', e => {
-    const elem = e.target;
-  
-    const addToBtn = elem.closest('.buttons__btn--add-to');
-    const removeBtn = elem.closest('.fav-buttons__btn--remove')
-    if (elem.classList.contains('buttons__btn--learn-more')) {
-      handleOpenCloseModal(e);
-    }
-    if (addToBtn) {
-      addToBtn.classList.remove('buttons__btn--add-to');
-      addToBtn.classList.add('fav-buttons__btn--remove');
-  
-      addToBtn.innerHTML = `Remove<svg class="buttons__icon"><use href="${icons}#heart"></use></svg>`;
-      favGallery.addToFavorite(addToBtn);
-    }
-    if (removeBtn) {
-      console.log('removeBtn: ', removeBtn);
-      removeBtn.classList.remove('fav-buttons__btn--remove')
-      removeBtn.classList.add('buttons__btn--add-to');
-  
-      removeBtn.innerHTML = `Add to<svg class="buttons__icon"><use href="${icons}#heart"></use></>`;
-      favGallery.removeFromFavoriteFromGallery(removeBtn);
-    }
-  });
-// +++++++++++++++++    // 
+    // +++++++++++++++++     //
+    galleryEl.addEventListener('click', e => {
+      const elem = e.target;
+
+      const addToBtn = elem.closest('.buttons__btn--add-to');
+      const removeBtn = elem.closest('.fav-buttons__btn--remove')
+      if (elem.classList.contains('buttons__btn--learn-more')) {
+        handleOpenCloseModal(e);
+      }
+      if (addToBtn) {
+        addToBtn.classList.remove('buttons__btn--add-to');
+        addToBtn.classList.add('fav-buttons__btn--remove');
+
+        addToBtn.innerHTML = `Remove<svg class="buttons__icon"><use href="${icons}#icon-heart_fill"></use></svg>`;
+        favGallery.addToFavorite(addToBtn);
+      }
+      if (removeBtn) {
+        console.log('removeBtn: ', removeBtn);
+        removeBtn.classList.remove('fav-buttons__btn--remove')
+        removeBtn.classList.add('buttons__btn--add-to');
+
+        removeBtn.innerHTML = `Add to<svg class="buttons__icon"><use href="${icons}#heart"></use></>`;
+        favGallery.removeFromFavoriteFromGallery(removeBtn);
+      }
+    });
+    // +++++++++++++++++    // 
   }
 }
 
