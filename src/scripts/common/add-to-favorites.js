db9d9c9943dbc69fb83408827e990a532b993f7e
Olga Polishchuk <olgapolichuk@ukr.net>
2023-03-11 16:52:46 +0200

    theme dark

--- src/scripts/common/add-to-favorites.js
+++ src/scripts/common/add-to-favorites.js
@@ -1,6 +1,7 @@
 const galleryEl = document.querySelector('.gallery');
 // body.addEventListener('click', handleClickAddToFavIngr);
 const LS_KEY_FAV_COCKT = 'Fav-Cocktails';
+// galleryEl.addEventListener('.addToFavHandlerN');
 
 export function addToFavHandler() {
     const id = this.attributes['data-id'].value
@@ -17,42 +18,51 @@ export function addToFavHandler() {
 
 }
 
-export function addToFavHandlerN(e) {
-    if (!e.target.classList.contains('js-cockt-add') 
-    && !e.target.classList.contains('js-cockt-remove') 
-    && e.target.classList.contains('close-modal')) {
-      return;
-    }
+// export function addToFavHandlerN(e) {
+//     if (!e.target.classList.contains('js-cockt-add') 
+//     && !e.target.classList.contains('js-cockt-remove') 
+//     && e.target.classList.contains('close-modal')) {
+//       return;
+//     }
   
-    if (e.target.classList.contains('js-cockt-add')) {
-      const favCocktails = JSON.parse(localStorage.getItem(LS_KEY_FAV_COCKT));
+//     if (e.target.classList.contains('js-cockt-add')) {
+//       const favCocktails = JSON.parse(localStorage.getItem(LS_KEY_FAV_COCKT));
   
-      if(!favCocktails) {
-        localStorage.setItem(LS_KEY_FAV_COCKT, JSON.stringify([]));
-        addToFavCoctails(e);
-        return
-      }
-      addToFavCoctails(e);
-      return
-    }
+//       if(!favCocktails) {
+//         localStorage.setItem(LS_KEY_FAV_COCKT, JSON.stringify([]));
+//         addToFavCoctails(e);
+//         return
+//       }
+//       addToFavCoctails(e);
+//       return
+//     }
   
-    if (e.target.classList.contains('js-cockt-remove')) {
-      const favCocktails = JSON.parse(localStorage.getItem(LS_KEY_FAV_COCKT));
+//     if (e.target.classList.contains('js-cockt-remove')) {
+//       const favCocktails = JSON.parse(localStorage.getItem(LS_KEY_FAV_COCKT));
   
-      let favIngrID = e.target.closest('.ing-wrapper').id;
+//       let favCocktailID = e.target.closest('.js-card').id;
   
-      const idx = favCocktails.findIndex(ing => ing === favIngrID);
+//       const idx = favCocktails.findIndex(ing => ing === favCocktailID);
   
-      favCocktails.splice(idx, 1);
+//       favCocktails.splice(idx, 1); //  переписати на сорт!!!!
   
-      localStorage.setItem(LS_KEY_FAV_COCKT, JSON.stringify(favCocktails));
-      e.target.classList.add('js-ing-add');
-      e.target.classList.remove('js-ing-remove');
-      e.target.textContent = 'Add to favorite';
-      return;
-    }
-    if (e.target.classList.contains('close-modal2')) {
-      body.removeEventListener('click', handleClickAddToFavIngr);
-      return
-    }
-  }
+//       localStorage.setItem(LS_KEY_FAV_COCKT, JSON.stringify(favCocktails));
+//       e.target.classList.add('js-cockt-add');
+//       e.target.classList.remove('js-cockt-remove');
+//       e.target.textContent = 'Add to';
+//       return;
+//     }
+//     // if (e.target.classList.contains('close-modal')) {
+//     //   body.removeEventListener('click', handleClickAddToFavIngr);
+//     //   return
+//     // }
+//   }
+//   function addToFavCocktails(e) {
+//     const favCocktails = JSON.parse(localStorage.getItem(LS_KEY_FAV_COCKT));
+//     let favCocktailID = e.target.closest('.js-card').id;
+//     favCocktails.push(favCocktailID);
+//     localStorage.setItem(LS_KEY_FAV_COCKT, JSON.stringify(favCocktails));
+//     e.target.classList.add('js-cockt-remove');
+//     e.target.classList.remove('js-cockt-add');
+//     e.target.textContent = 'Remove';
+//   }
